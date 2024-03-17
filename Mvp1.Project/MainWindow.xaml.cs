2ee51aa8922b2754dd0462183d3c18e955225e86
GeorgeBacalu <georgebacalu83@gmail.com>
2024-03-17 13:13:57 +0200

    Prepare setup for modules development

--- Mvp1.Project/MainWindow.xaml.cs
+++ Mvp1.Project/MainWindow.xaml.cs
@@ -1,12 +1,18 @@
-﻿using System.Windows;
+﻿using Mvp1.Project.Modules.Administrative;
+using Mvp1.Project.Modules.WordFinder;
+using Mvp1.Project.Modules.Entertainment;
+using System.Windows;
 
 namespace Mvp1.Project
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
+
+        private void ButtonAdministrative_Click(object sender, RoutedEventArgs e) => new AdministrativeModule().Show();
+
+        private void ButtonWordFinder_Click(object sender, RoutedEventArgs e) => new WordFinderModule().Show();
+
+        private void ButtonEntertainment_Click(object sender, RoutedEventArgs e) => new EntertainmentModule().Show();
     }
 }
